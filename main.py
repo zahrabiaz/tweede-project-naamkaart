685c7bb0027b418e8d3a319db62d92a72a432b96
zahrabiaz <122613734+zahrabiaz@users.noreply.github.com>
2023-01-13 13:25:01 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,7 @@
+def naamkaartje():
+    basic.show_string("Mijn naam is ")
+    basic.show_string("Zahra")
+    basic.show_string("Mijn leeftijd is")
+    basic.show_number(16)
+
+basic.forever(naamkaartje)
---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function naamkaartje() {
+    basic.showString("Mijn naam is ")
+    basic.showString("Zahra")
+    basic.showString("Mijn leeftijd is")
+    basic.showNumber(16)
+})
